08ad18e5d1a526184e0097591a19edf0ab808f75
Ernesto Freyre <ernestofreyreg@gmail.com>
2023-03-08 23:01:52 +0000

    added components folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,11 @@
-import Image from "next/image";
+import { Footer } from "@/components/Footer";
+import { Welcome } from "@/components/Welcome";
 
 export default function Home() {
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">Hello Rigo!</h1>
-      <p>
-        <Image
-          src="/images/rigo-baby.jpeg"
-          alt="4geeks"
-          width={300}
-          height={254}
-        />
-      </p>
-      <a href="#" className="btn btn-success">
-        If you see this green button... bootstrap is working...
-      </a>
-      <p>
-        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-        love!
-      </p>
+    <div className="container">
+      <Welcome />
+      <Footer />
     </div>
   );
 }
